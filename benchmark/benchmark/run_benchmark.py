c5073cfe77ec514f9f64ebf759b1527ec2ae4c12
Richard Blythman <richardblythman@gmail.com>
2024-01-22 11:39:38 +0000

    save errors

--- benchmark/benchmark/run_benchmark.py
+++ benchmark/benchmark/run_benchmark.py
@@ -81,6 +81,8 @@ def run_benchmark(kwargs):
             "input_cost",
             "output_cost",
             "total_cost",
+            "prompt_response",
+            "error"
         ]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
 
@@ -101,55 +103,55 @@ def run_benchmark(kwargs):
 
                 CURRENT_RETRIES = 0
 
-                while True:
-                    try:
-                        tool = tool_map(t)
-                        response = tool.run(**{**test_q, **kwargs})
-                        result = json.loads(response[0])
-                        test_q["p_yes"] = float(result["p_yes"])
-                        test_q["p_no"] = float(result["p_no"])
-                        if response[2] is not None:
-                            test_q["input_tokens"] = response[2].input_tokens
-                            test_q["output_tokens"] = response[2].output_tokens
-                            test_q["total_tokens"] = response[2].total_tokens
-                            test_q["input_cost"] = response[2].input_cost
-                            test_q["output_cost"] = response[2].output_cost
-                            test_q["total_cost"] = response[2].total_cost
-                        # test_q["prompt_response"] = response[1].replace(os.linesep, "")
+                # while True:
+                try:
+                    tool = tool_map(t)
+                    response = tool.run(**{**test_q, **kwargs})
+                    result = json.loads(response[0])
+                    test_q["p_yes"] = float(result["p_yes"])
+                    test_q["p_no"] = float(result["p_no"])
+                    if response[2] is not None:
+                        test_q["input_tokens"] = response[2].input_tokens
+                        test_q["output_tokens"] = response[2].output_tokens
+                        test_q["total_tokens"] = response[2].total_tokens
+                        test_q["input_cost"] = response[2].input_cost
+                        test_q["output_cost"] = response[2].output_cost
+                        test_q["total_cost"] = response[2].total_cost
+                        test_q["prompt_response"] = response[1].replace(os.linesep, "")
 
-                        if float(result["p_yes"]) == float(result["p_no"]):
-                            test_q["prediction"] = "undecided"
-                        else:
-                            test_q["prediction"] = (
-                                "yes" if test_q["p_yes"] > test_q["p_no"] else "no"
-                            )
+                    if float(result["p_yes"]) == float(result["p_no"]):
+                        test_q["prediction"] = "undecided"
+                    else:
+                        test_q["prediction"] = (
+                            "yes" if test_q["p_yes"] > test_q["p_no"] else "no"
+                        )
 
-                        test_q["Correct"] = test_q["prediction"] == test_q["answer"]
+                    test_q["Correct"] = test_q["prediction"] == test_q["answer"]
 
-                        del test_q["source_links"]
-                        del test_q["counter_callback"]
 
-                        writer.writerow(test_q)
+                except openai.error.OpenAIError as e:
+                    logger.error(f"Error running benchmark for tool {t}: {e}")
+                    CURRENT_RETRIES += 1
+                    if CURRENT_RETRIES > MAX_RETRIES:
+                        logger.error(
+                            f"Max retries reached for tool {t}. Skipping question."
+                        )
+                        test_q["error"] = e
+                    else:
+                        logger.info(
+                            f"Retrying tool {t} for question {test_q['prompt']}"
+                        )
 
-                        break
+                except Exception as e:
+                    logger.error(f"Error running benchmark for tool {t}: {e}")
+                    test_q["error"] = e
 
-                    except openai.error.OpenAIError as e:
-                        logger.error(f"Error running benchmark for tool {t}: {e}")
-                        CURRENT_RETRIES += 1
-                        if CURRENT_RETRIES > MAX_RETRIES:
-                            logger.error(
-                                f"Max retries reached for tool {t}. Skipping question."
-                            )
-                            break
-                        else:
-                            logger.info(
-                                f"Retrying tool {t} for question {test_q['prompt']}"
-                            )
+                del test_q["source_links"]
+                del test_q["counter_callback"]
 
-                    except Exception as e:
-                        logger.error(f"Error running benchmark for tool {t}: {e}")
-                        break
+                writer.writerow(test_q)
 
+ 
     results_df = pd.read_csv(csv_file_path)
     grouped_df = results_df.groupby("tool").agg(
         {
@@ -160,6 +162,7 @@ def run_benchmark(kwargs):
             "input_cost": ["mean"],
             "output_cost": ["mean"],
             "total_cost": ["mean"],
+            "error": "count"  # Count non-NaN values in the 'error' column
         }
     )
 
