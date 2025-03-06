f6b6b602c6a1d710ee63bc3f6635c4e1e3a0b6f0
cyberosa <arosacastillo@protonmail.com>
2025-03-06 12:47:55 +0100

    Removing Claude 3 Sonnet model

--- benchmark/benchmark/run_benchmark.py
+++ benchmark/benchmark/run_benchmark.py
@@ -313,7 +313,6 @@ if __name__ == "__main__":
     kwargs["llm_provider"] = "openrouter"
     kwargs["model"] = [  # only supports running for one model (takes first in list)
         # "claude-3-haiku-20240307",
-        # "claude-3-sonnet-20240229",
         # "claude-3-opus-20240229",
         # "gpt-3.5-turbo-0125",
         # "gpt-4-0125-preview",
